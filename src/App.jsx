9b72977c9217415be1018c7c537c15e226db93a1
Ahrin <hinatairene99@gmail.com>
2025-07-30 02:30:18 +0800

    create /Components/data/questionsData

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import { questionsData } from './Components/data/questionsData';
 
 function App() {
   
---
+++ src/Components/data/questionsData.js
@@ -0,0 +1,126 @@
+import React from 'react';
+
+const questionsData = [
+  {
+    id: 1,
+    question: "17 rounded off to the nearest 10 is...",
+    options: [
+      { value: "10", label: "10" },
+      { value: "20", label: "20" },
+      { value: "17", label: "17" },
+    ],
+    correctAnswer: "20",
+  },
+  {
+    id: 2,
+    question: "45 rounded off to the nearest 10 is...",
+    options: [
+      { value: "50", label: "50" },
+      { value: "45", label: "45" },
+      { value: "40", label: "40" },
+    ],
+    correctAnswer: "50",
+  },
+  {
+    id: 3,
+    question: "75 rounded off to the nearest 10 is...",
+    options: [
+      { value: "70", label: "70" },
+      { value: "80", label: "80" },
+      { value: "175", label: "175" },
+    ],
+    correctAnswer: "80",
+  },
+  {
+    id: 4,
+    question: "19 rounded to the nearest 10 is...",
+    options: [
+      { value: "20", label: "20" },
+      { value: "10", label: "10" },
+      { value: "19", label: "19" },
+    ],
+    correctAnswer: "20",
+  },
+  {
+    id: 5,
+    question: "64 rounded off to the nearest 10 is...",
+    options: [
+      { value: "64", label: "64" },
+      { value: "70", label: "70" },
+      { value: "60", label: "60" },
+    ],
+    correctAnswer: "60",
+  },
+  {
+    id: 6,
+    question: "0 rounded off to the nearest 10 is...",
+    options: [
+      { value: "10", label: "10" },
+      { value: "1", label: "1" },
+      { value: "0", label: "0" },
+    ],
+    correctAnswer: "0",
+  },
+  {
+    id: 7,
+    question: "98 rounded off to the nearest 10 is...",
+    options: [
+      { value: "80", label: "80" },
+      { value: "100", label: "100" },
+      { value: "89", label: "89" },
+    ],
+    correctAnswer: "100",
+  },
+  {
+    id: 8,
+    question: "199 rounded off to the nearest 10 is...",
+    options: [
+      { value: "190", label: "190" },
+      { value: "100", label: "100" },
+      { value: "200", label: "200" },
+    ],
+    correctAnswer: "200",
+  },
+  {
+    id: 9,
+    question: "94 rounded off to the nearest 10 is...",
+    options: [
+      { value: "100", label: "100" },
+      { value: "94", label: "94" },
+      { value: "90", label: "90" },
+    ],
+    correctAnswer: "90",
+  },
+  {
+    id: 10,
+    question: "165 rounded off to the nearest 10 is...",
+    options: [
+      { value: "160", label: "160" },
+      { value: "170", label: "170" },
+      { value: "150", label: "150" },
+    ],
+    correctAnswer: "170",
+  },
+  {
+    id: 11,
+    question: "445 rounded off to the nearest 10 is...",
+    options: [
+      { value: "450", label: "450" },
+      { value: "440", label: "440" },
+      { value: "500", label: "500" },
+    ],
+    correctAnswer: "450",
+  },
+  {
+    id: 12,
+    question: "999 rounded off to the nearest 10 is...",
+    options: [
+      { value: "990", label: "990" },
+      { value: "1,000", label: "1,000" },
+      { value: "909", label: "909" },
+    ],
+    correctAnswer: "1,000",
+  },
+];
+
+export default questionsData;
