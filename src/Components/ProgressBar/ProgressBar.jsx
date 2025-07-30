583fc47b434783b5f8b7b369f48df5bc2c05d64e
Ahrin <hinatairene99@gmail.com>
2025-07-30 21:13:56 +0800

    update ProgressBar.jsx

--- src/Components/ProgressBar/ProgressBar.jsx
+++ src/Components/ProgressBar/ProgressBar.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const ProgressBar = () => {
+const ProgressBar = ({answered, total}) => {
     return (
-        
-    )
+        <div className="progress-container">
+            <div className="progress-text">
+                Progress: {answered}/{total} completed
+            </div>
+            <div className="progress-bar">
+                <div
+                className="progress-fill"
+                style={{ width: `${progressPercentage}%` }}
+                >
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ProgressBar;
