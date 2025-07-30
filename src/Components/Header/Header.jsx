4f9072a94f3c0e3e08bc5a95ee9c152ef49f2e66
Ahrin <hinatairene99@gmail.com>
2025-07-30 21:06:52 +0800

    added ProgressBar & NameInput component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,6 +6,8 @@ const Header = () => {
         <header className="headerSticky-top">
             <h1 className="title">Rounding Off to Nearest 10</h1>
 
+            <ProgressBar/>
+            <NameInput/>
 
         </header>
     );
